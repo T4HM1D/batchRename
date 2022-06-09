def8ddb5e85320c1eaa641483cd0c050f9b485e3
Tahmidol Alom <32310776+T4HM1D@users.noreply.github.com>
2022-06-09 09:53:35 +0100

    updated

--- rename.py
+++ rename.py
@@ -15,10 +15,8 @@ for files in videoPath.iterdir():
 fileList.sort()
 
 for f in fileList:
-    # f1, f2, f3, ep = f.stem.split('-')
-    # nameTemplate = '{}-{}-{}-{}{}'.format(f1.capitalize(),f2.capitalize(), f3.capitalize(), ep.upper(), f.suffix)
     fName1, fName2, fEp, fRes, fRip, fSource, fExt = f.name.split('.')
-    nameTemplate = '{}-{}-{}.{}'.format(fName1.capitalize(),
-                                        fName2.capitalize(), fEp.upper(), fExt)
+    f1, f2 = fName1.split('-')
+    nameTemplate = f'{f1.capitalize()}-{f2.capitalize()}-{fName2.capitalize()}-{fEp.upper()}.{fExt}'
     print(nameTemplate)
     f.rename(nameTemplate)
